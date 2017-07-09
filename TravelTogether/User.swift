f3f9ba5c2b384c357d9c949b3b749762f06cfbae
Dmitry <DmitryMac@iMac-Dmitry.local>
2017-07-09 22:37:32 +0500

    неадаптированный чат

--- TravelTogether/User.swift
+++ TravelTogether/User.swift
@@ -18,4 +18,14 @@ struct User {
     static var firstTravel: String?
     static var secondTravel: String?
     static var thirdTravel: String?
+    var id: String?
+    var name: String?
+    var email: String?
+    var profileImageUrl: String?
+    init(dictionary: [String: AnyObject]) {
+        self.id = dictionary["id"] as? String
+        self.name = dictionary["name"] as? String
+        self.email = dictionary["email"] as? String
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String
+    }
 }
